d7f27ce4307b4efb9d4b03a23ab9af10055b3336
Tim Peters <timothy.peters@turner.com>
2017-06-01 09:12:03 -0400

    cleanup

--- src/lambda/lambdaSendText.js
+++ src/lambda/lambdaSendText.js
@@ -1,24 +1,9 @@
 'use strict';
 
-/**
- * This is a sample Lambda function that sends an Email on click of a
- * button. It creates a SNS topic, subscribes an endpoint (EMAIL)
- * to the topic and publishes to the topic.
- *
- * Follow these steps to complete the configuration of your function:
- *
- * 1. Update the email environment variable with your email address.
- * 2. Enter a name for your execution role in the "Role name" field.
- *    Your function's execution role needs specific permissions for SNS operations
- *    to send an email. We have pre-selected the "AWS IoT Button permissions"
- *    policy template that will automatically add these permissions.
- */
-
 const AWS = require('aws-sdk');
 
 const EMAIL = 'timothy_peters@yahoo.com';
 const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
-
 
 function findExistingSubscription(topicArn, nextToken, cb) {
     const params = {
@@ -124,8 +109,8 @@ exports.handler = (event, context, callback) => {
         console.log(`Publishing to topic ${topicArn}`);
         // publish message
         const params = {
-            Message: `${event.serialNumber} -- processed by Lambda\nBattery voltage: ${event.batteryVoltage}`,
-            Subject: `Hello from your IoT Button ${event.serialNumber}: ${event.clickType}`,
+            Message: `Sent from Lambda, dude`,
+            Subject: `Subject from Lambda`,
             TopicArn: topicArn,
         };
         // result will go to function callback
